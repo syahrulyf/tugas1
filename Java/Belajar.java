766a795936e8ebce1152dbae78027d4173904168
Syahrul <61736792+syahrulyf@users.noreply.github.com>
2020-03-12 18:11:26 +0800

    Add files via upload

---
+++ Java/Belajar.java
@@ -0,0 +1,7 @@
+public class Belajar{
+
+ public static void main(String[] args) {
+		System.out.print("Belajar Bahasa Pemrograman Java..");
+
+	}
+}
---
+++ Java/Selesai.java
@@ -0,0 +1,7 @@
+public class Selesai{
+
+ public static void main(String[] args) {
+		System.out.print("Selesai Belajar Pemrograman Java..");
+
+	}
+}
